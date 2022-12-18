8f0504b192796a00e4d52709b56da5bc0c833e1c
FabioCircle <zzzfwww@163.com>
2022-12-18 21:07:46 +0800

    vue demo

---
+++ vue-demo/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h3>Study Vue</h3>
+    <p>{{ message}}</p>
+    <div>{{ rawHtml}}</div>
+    <div v-html="rawHtml"></div>
+    <div :id="dynamicId"></div>
+    <p> {{ num+100 }}</p>
+    <p> {{flag ? "美眉":"抠脚汉"}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      message: "Day Day up",
+      rawHtml: "<a href='https://www.baidu.com'>Baidu</a>",
+      dynamicId: 8888,
+      num: 100,
+      flag: false
+    }
+  }
+}
+</script>
+
