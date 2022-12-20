87235e1c4d76cea6f68ba49b44d196efeeb6e1fa
FabioCircle <zzzfwww@163.com>
2022-12-20 22:20:43 +0800

    component lifecycle and swiper

---
+++ component-lifecycle/README.md
@@ -0,0 +1,40 @@
+# component-lifecycle
+
+## cnpm安装
+```sh
+# 使用淘宝源，便于国内网络使用
+npm install -g cnpm --registry=https://registry.npmmirror.com
+```
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### swiper使用
+
+#### 下载依赖
+```sh
+# 下载最新版本
+cnpm install --save swiper
+#
+# cat package.json
+#"swiper": "^8.4.5",
+#
+# 下载指定版本
+cnpm install --save swiper@8.1.6
+```
---
+++ component-lifecycle/src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <Swiper :modules="modules" :pagination="{ clickable:true}">
+      <SwiperSlide>
+        <img src="../assets/1.jpg" alt="">
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="../assets/2.jpeg" alt="">
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="../assets/1.jpg" alt="">
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="../assets/2.jpeg" alt="">
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<script>
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import { Pagination } from 'swiper';
+import 'swiper/css/pagination';
+
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      modules: [Pagination]
+    }
+  },
+  components:{
+    Swiper,
+    SwiperSlide
+  },
+
+  beforeCreate(){
+    console.log("beforeCreate:组件创建之前");
+  },
+  created(){
+    console.log("created:组件创建完成");
+  },
+  beforeMount(){
+    console.log("beforeMount:组件渲染之前");
+  },
+  mounted(){
+    console.log("mounted:组件渲染完成");
+    // 把网络请求放到这里
+  },
+  beforeUpdate(){
+    console.log("beforeUpdate:组件更新之前");
+  },
+  updated(){
+    console.log("updated:组件更新之后");
+  },
+  beforeUnmount(){
+    console.log("beforeUnmount:组件卸载之前");
+    // 卸载之前，把消耗性能的处理都完成
+    // 定时器
+  },
+  unmounted(){
+    console.log("unmounted:组件卸载之后");
+  }
+}
+</script>
+
+<style scoped>
+img{
+  width: 100%;
+}
+</style>
